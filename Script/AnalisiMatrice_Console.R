24f4f169bc1ee111d9fecbe482ee7642ee9f1282
alesuper6 <a.chiappelloni@campus.unimib.it>
2024-07-29 16:05:50 +0200

    Inclusa la visualizzazione del grafico risultante tramite il comando da console

--- Script/AnalisiMatrice_Console.R
+++ Script/AnalisiMatrice_Console.R
@@ -8,5 +8,14 @@ n <- as.integer(args[1])  # converte il primo parametro in un intero
 
 source("~/Calinski_Harabasz_Thesis/Script/FunzioniMatrice.R")
 
-AnalisiMatrice(n)
+# Chiamata funzione per l'analisi del dataset artificiale
+file_name <- AnalisiMatrice(n, TRUE)
 
+# Percorso del file in formato Unix
+unix_path <- file_name
+
+# Converti il percorso del file in formato Windows
+windows_path <- normalizePath(unix_path, winslash = "\\")
+
+# Apri il file PDF 
+system2("open" , windows_path)
