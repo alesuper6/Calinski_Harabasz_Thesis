eab1b7424878471c95f80abf306cc5cdeeac6cd2
alesuper6 <a.chiappelloni@campus.unimib.it>
2024-07-29 16:07:53 +0200

    Eliminata funzione deprecata. Modifica sistema di visualizzazione del grafico risultante dall'analisi

--- Script/FunzioniMatrice.R
+++ Script/FunzioniMatrice.R
@@ -1,3 +1,7 @@
+library(ggplot2)
+library(fpc)
+
+
 ## Funzione che crea la matrice parametrica [2n, n]
 ## Con la prima metà di righe di soli 0 e l'altra metà di righe di soli 1
 
@@ -16,29 +20,6 @@ creaMatrice <- function(n) {
 
 ##---------------------------------------------------------------------------##
 
-## Funzione che modifica la matrice passata
-## Sostituisce casualmente un numero x di righe con valori compresi tra 0 e 1
-
-rimpiazzaCasualmenteRighe <- function(matr, x) {
-  # Ottieni le dimensioni della matrice
-  nrighe <- nrow(matr)
-  ncolonne <- ncol(matr)
-  
-  # Controlla se x è valido
-  if (x > nrighe) {
-    stop("x non può essere maggiore del numero di righe della matrice")
-  }
-  
-  # Seleziona x righe casuali
-  righe_casuali <- sample(1:nrighe, x)
-  
-  # Sostituisci le righe casuali con numeri casuali tra 0 e 1
-  matr[righe_casuali, ] <- matrix(runif(x * ncolonne), nrow = x)
-  
-  return(matr)
-}
-
-##---------------------------------------------------------------------------##
 
 ## Funzione che modifica la matrice passata
 ## Sostituisce le righe selezionate con valori compresi tra 0 e 1
@@ -59,6 +40,7 @@ modificaRigaSelezionata <- function(matrice, riga) {
 ##---------------------------------------------------------------------------##
 
 # Definizione di una funzione per leggere un intero da tastiera
+
 readInteger <- function() {
   n <- readline(prompt="Inserisci un intero: ")
   return(as.integer(n))
@@ -66,8 +48,124 @@ readInteger <- function() {
 
 ##---------------------------------------------------------------------------##
 
+# Funzione per iterazione dei passaggi di modifica della matrice e validazione attraverso CH
+# Il parametro console viene settato a False di base per le chiamate eseguite all'interno di Rstudio
+
+AnalisiMatrice <- function(n, console=FALSE){
+  
+  # CASO BASE: prima metà delle righe composta di soli 0 e la seconda metà 
+  # composta di soli 1.
+  
+  # Abilita/disabilita stampa
+  verbose <- FALSE
+  
+  # controlli del parametro n per la matrice
+  if(missing(n))
+    stop("Missing input argument. A parametric value is required")
+  if(!is.numeric(n))
+    stop("Argument 'n' must be numeric")
+  if(n<=1)
+    warning("The minimum value for consideration should be higher than 1",immediate. = TRUE)
+  
+  if(verbose){
+    print(n)
+  }
+  # Creazione e visualizzazione matrice 
+  matrice <-creaMatrice(n)
+  if(verbose){
+    matrice
+  }
+  
+  
+  # Clustering con K-means
+  cl <- kmeans(matrice, 2, , 100)
+  
+  # Calcolo dell'indice di Calinski-Harabasz con calinhara
+  # Valore atteso -> inf
+  CH <- calinhara(matrice, cl$cluster)
+  cat(sprintf("\nValore indice Calinski-Harabasz: %f\n", CH))
+  
+  
+  # CASO ITERATIVO: viene presa la matrice del caso precedente ed una riga scelta 
+  #                 casualmente viene sostituita con valori compresi tra 0 e 1.
+  
+  
+  
+  # Creazione dataframe iterazioni/valore
+  risultati <- data.frame(iterazione = integer(), valore = numeric(),
+                          stringsAsFactors = FALSE)
+  
+  
+  # Creazione pool da cui estrarre la riga da modificare
+  m <- nrow(matrice) # Numero di righe della matrice
+  pool <- sample(1:m) # Crea un pool di numeri da 1 a n ordinato in modo casuale
+  
+  
+  # Ciclo iterativo per modifica di una riga alla volta
+  for (i in 1:nrow(matrice)) {
+    if(verbose){
+      cat(sprintf("\nIterazione %d:\n", i))
+    }
+    
+    # Pesca un numero casualmente senza reimmissione dal pool creato
+    selezione <- pool[i] 
+    if(verbose){
+      cat(sprintf("\nRiga modificata: %d\n", selezione))
+    }
+    
+    # Modifica riga della matrice
+    matrice <- modificaRigaSelezionata(matrice, selezione)
+    
+    # Visualizzazione della matrice
+    if(verbose){
+      print(matrice)
+    }
+    
+    
+    # Clustering con K-means
+    cl <- kmeans(matrice, 2, , 100)
+    
+    # Calcolo dell'indice di Calinski-Harabasz con calinhara
+    CH <- calinhara(matrice, cl$cluster)
+    cat(sprintf("\nValore indice Calinski-Harabasz: %f\n", CH))
+    
+    # Aggiornamento dataframe iterazioni/valore
+    risultati <- rbind(risultati, data.frame(iterazione = i, valore = CH))
+    
+  }
+  
+  
+  # Visualizzazione dei valori di Calinski-Harabasz per ogni matrice
+  print(risultati)
+  
+  
+  
+  # Creazione del grafico a linee
+  grafico <- ggplot(data = risultati, aes(x = risultati[,1] , y = risultati[,2])) +
+    geom_line(color = "blue", linewidth = 1) +
+    ylim(0, max(risultati$valore)) +
+    xlim(0, 2*n) +
+    theme(panel.grid.major = element_line(color = "grey"), # Aggiunge la griglia principale
+          panel.grid.minor = element_blank(), # Rimuove la griglia secondaria
+          panel.background = element_blank()) +
+    scale_x_continuous(breaks = round(seq(min(risultati$iterazione), max(risultati$iterazione), by = 1), 1)) +
+    labs(title = "Grafico valore CH", x = "# righe manipolate", y = "Calinski-Harabasz index")
+  
+  
+  # Visualizzazione del grafico
+  visualizzaGrafico(grafico, n, console)
+  
+  
+}
+
+
+##---------------------------------------------------------------------------##
+
+
 #Visualizzazione grafico della manipolazione della matrice
-visualizzaGrafico <- function(grafico, n) {
+
+visualizzaGrafico <- function(grafico, n, console) {
+  
   # Percorso della directory di download
   download_dir <- "~/Calinski_Harabasz_Thesis/Risultati/GraficiDatasetArtificiali" 
   
@@ -101,12 +199,12 @@ visualizzaGrafico <- function(grafico, n) {
     ggsave(paste0(base_name, n, ext), plot = grafico, device = NULL, path = "~/Calinski_Harabasz_Thesis/Risultati/GraficiDatasetArtificiali")
   }
   
-  # Percorso del file in formato Unix
-  unix_path <- file_name
+  # Se la funzione viene chiamata da console restituisce il percorso del file 
+  if(console){
+    return(file_name)
+  }
+  else{
+    grafico
+  }
   
-  # Converti il percorso del file in formato Windows
-  windows_path <- normalizePath(unix_path, winslash = "\\")
-  
-  # Apri il file PDF 
-  system2("open" , windows_path)
 }
